8938e475e3a36308b8ebd96f1a0adf8ab073b5b1
Liandjelo <146956676+Liandjelo@users.noreply.github.com>
2024-06-01 22:49:05 +0800

    Update loading-screen.js

--- scripts/loading-screen.js
+++ scripts/loading-screen.js
@@ -6,3 +6,11 @@ window.addEventListener('load',()=>{
 function hideElements(x){
         document.getElementById(x).display = 'none';
 }
+
+window.addEventListener('beforeunload',()=> {
+        var a = document.getElementsByClassName("button");
+
+        Array.from(a).forEach(e=> {
+                e.style.display = 'none';
+        });
+});
